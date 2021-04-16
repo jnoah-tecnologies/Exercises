c4bbd1c9a1fa2fdd62730afe1d66ee827971b841
Eizo_Okada <eizo_okada@jnoah.co.jp>
2021-04-16 12:25:27 +0900

    一度に全てコンパイルするスクリプト作成 gitignoreを作成して*.outを無視するように

---
+++ src/compile.sh
@@ -0,0 +1,28 @@
+# Pointers
+gcc -o Pointers/sample1.out Pointers/sample1.c
+gcc -o Pointers/sample2.out Pointers/sample2.c
+
+
+# Relationship_Between_Arrays_and_Pointers
+gcc -o Relationship_Between_Arrays_and_Pointers/sample1.out Relationship_Between_Arrays_and_Pointers/sample1.c
+gcc -o Relationship_Between_Arrays_and_Pointers/sample2.out Relationship_Between_Arrays_and_Pointers/sample2.c
+gcc -o Relationship_Between_Arrays_and_Pointers/sample3.out Relationship_Between_Arrays_and_Pointers/sample3.c
+
+
+# Pass_Addresses_and_Pointers
+gcc -o Pass_Addresses_and_Pointers/sample1.out Pass_Addresses_and_Pointers/sample1.c
+gcc -o Pass_Addresses_and_Pointers/sample2.out Pass_Addresses_and_Pointers/sample2.c
+
+
+# Dynamic_Memory_Allocation
+gcc -o Dynamic_Memory_Allocation/sample1.out Dynamic_Memory_Allocation/sample1.c
+gcc -o Dynamic_Memory_Allocation/sample2.out Dynamic_Memory_Allocation/sample2.c
+gcc -o Dynamic_Memory_Allocation/sample3.out Dynamic_Memory_Allocation/sample3.c
+
+
+# Pointer_to_Pointer
+gcc -o Pointer_to_Pointer/sample1.out Pointer_to_Pointer/sample1.c
+
+
+# Function_Pointer
+gcc -o Function_Pointer/sample1.out Function_Pointer/sample1.c
