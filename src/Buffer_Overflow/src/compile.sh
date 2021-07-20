a4bb5d33654a25dbf934f50c5be8cb95aa21db2e
jnoah-hioki <satoshi_hioki@jnoah.co.jp>
2021-07-20 07:47:15 +0000

    bof

--- src/Buffer_Overflow/src/compile.sh
+++ src/Buffer_Overflow/src/compile.sh
@@ -1,2 +1,3 @@
-gcc -z norelro -z execstack -no-pie -fno-stack-protector -o ../bof bof.c
+gcc -Wl,-z,norelro -z execstack -no-pie -fno-stack-protector -o ../bof bof.c
+gcc -Wl,-z,norelro -z execstack -no-pie -o ../bof2 bof.c
 
