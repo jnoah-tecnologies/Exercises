78b4e946a963762b3268b7afde03505967bf9605
Ubuntu <ubuntu@ip-172-30-2-91.ap-northeast-1.compute.internal>
2021-09-15 01:08:15 +0000

    update docker push

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -1,8 +1,11 @@
 docker push jnoahdevelop/dev32
 docker push jnoahdevelop/dev64
 docker push jnoahdevelop/network
+
 docker push jnoahdevelop/attack
+docker push jnoahdevelop/attack_sonsole
 
 docker push jnoahdevelop/telnetd
 docker push jnoahdevelop/dvwa 
 docker push jnoahdevelop/dolibarr 
+
